b9f7d1d463a204dd91b42b90f74563334a2e1de8
Sweet Jimmy <james@james-knapp.com>
2025-11-09 17:34:17 -0500

    filetype change

---
+++ eslint.config.mjs
@@ -0,0 +1,43 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+  },
+])
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint','react','react-hooks','import'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'prettier'
+  ],
+  settings: { react: { version: 'detect' }, 'import/resolver': { typescript: true } },
+  rules: {
+    'import/order': ['warn', { 'newlines-between': 'always', alphabetize: { order: 'asc' } }],
+    '@typescript-eslint/consistent-type-imports': 'warn',
+    'react/react-in-jsx-scope': 'off'
+  }
+};
